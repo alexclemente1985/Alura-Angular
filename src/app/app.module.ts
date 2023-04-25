ddfe5745f37dba6e7a1868e2e363d6448a73e8b1
Alexandre Pinheiro <alexclemente1985@gmail.com>
2023-04-24 23:13:37 -0300

    limpeza de app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import { LikeWidgetComponent } from './shared/components/like-widget/like-widget.component';
 import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
-import { UniqueServiceId } from './shared/services/unique-id/unique-id.service';
 
 @NgModule({
   declarations: [
